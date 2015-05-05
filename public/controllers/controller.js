91ea188e536376b1c6dc7331e1e176cc09e1b668
Greg Wright <gregwrightbjj@gmail.com>
2015-05-04 23:29:41 -0400

    can add contacts to mongodb and refresh the page, add body-parser module

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,14 +3,22 @@ var myApp = angular.module("myApp", [])
 .controller("AppCtrl", ["$scope", "$http", function($scope, $http){
 	console.log("Hello World from controller");
 
-	$http.get("/contactList").success(function(response){
-		console.log("I go the data I requested")
-		$scope.contactList = response;
-	});
+	var refresh = function(){
+		$http.get("/contactList").success(function(response){
+			console.log("I go the data I requested")
+			$scope.contactList = response;
+			$scope.contact = "";
+		});
+	};
+	refresh();
 
 	$scope.addContact = function(){
-		console.log($scope.contact)
-	}
+		console.log($scope.contact);
+		$http.post("/contactList", $scope.contact).success(function(response){
+			console.log(response)
+			refresh();
+		});
+	};
 	
 	
 }]);
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require("express")
 var app = express();
 var mongojs = require("mongojs");
 var db = mongojs("contactList", ["contactList"])
+var bodyParser = require("body-parser")
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get("/contactList", function(req,res){
 	console.log("I recieved a get request");
@@ -11,6 +13,12 @@ app.get("/contactList", function(req,res){
 	db.contactList.find(function(err, docs){
 		console.log(docs)
 		res.json(docs)
+	})
+});
+app.post("/contactList", function(req, res){
+	console.log(req.body);
+	db.contactList.insert(req.body, function(err, doc){
+		res.json(doc);
 	})
 })
 
